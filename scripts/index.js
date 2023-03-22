25ea518d2b4905444adf672223bcd16e9d433bb3
Sergey <teprol112@bk.ru>
2023-03-22 04:40:55 +0400

    feat: рендер всех карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,47 @@
 // переменые
 
+// массив с карточками
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
 // попап
-let popup = document.querySelector(".popup");
-let buttonEditPopup = document.querySelector(".edit-button");
-let buttonClosePopup = popup.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
+const buttonEditPopup = document.querySelector(".edit-button");
+const buttonClosePopup = popup.querySelector(".popup__close");
 
 // форма
-let form = popup.querySelector(".popup__form");
-let popupInputName = popup.querySelector(".popup__input_name");
-let popupInputDescription = popup.querySelector(".popup__input_description");
+const form = popup.querySelector(".popup__form");
+const popupInputName = popup.querySelector(".popup__input_name");
+const popupInputDescription = popup.querySelector(".popup__input_description");
 
 // профиль
-let profile = document.querySelector(".profile");
-let profileName = profile.querySelector(".profile-info__name");
-let profileDescription = profile.querySelector(".profile-info__description");
+const profile = document.querySelector(".profile");
+const profileName = profile.querySelector(".profile-info__name");
+const profileDescription = profile.querySelector(".profile-info__description");
 
 // открытие попапа
 buttonEditPopup.addEventListener("click", function () {
@@ -28,10 +56,33 @@ buttonClosePopup.addEventListener("click", function () {
 });
 
 // отправка формы и сохранение текста в input
-form.addEventListener("submit", function(evt) {
+form.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
   profileName.textContent = popupInputName.value;
-  profileDescription.textContent =  popupInputDescription.value;
+  profileDescription.textContent = popupInputDescription.value;
   popup.classList.remove("popup_opened");
+});
+
+//* функция рендера карточки
+const renderCard = function (element) {
+  // список карточек
+  const listCards = document.querySelector(".elements__list");
+
+  // карточки
+  const cardTemplate = document.querySelector("#cards").content;
+  const card = cardTemplate.querySelector(".elements__item").cloneNode(true);
+  const image = card.querySelector(".element__image");
+  const title = card.querySelector(".element__title");
+
+  image.src = element.link;
+  title.textContent = element.name;
+
+  listCards.append(card);
+};
+
+// создадим карточки из массива
+initialCards.forEach((item) => {
+  //! сюда вставляем функцию которая будет рендерить карточку с каждым элементом массива
+  renderCard(item);
 });
