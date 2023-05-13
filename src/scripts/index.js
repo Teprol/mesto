39d5ffc89d3664cc036a6fb1837b34cd47c123b1
Sergey <teprol112@bk.ru>
2023-05-13 16:29:42 +0400

    fix/reviu1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,21 +14,20 @@ const formValidPopupProfile = new FormValidator(popupProfile, validationConfig);
 const formValidpopupCard = new FormValidator(popupCard, validationConfig);
 
 //* экземпляр класса профиля который не форма
-const UserProfile = new UserInfo(objectSelectors);
+const userProfile = new UserInfo(objectSelectors);
 
 //! экземпляр класса попап профиля
 const popupProfile1 = new PopupWithForm('.popup_profile', (evt) => {
   evt.preventDefault();
-  UserProfile.setUserInfo(popupProfile1.getInputValues());
+  userProfile.setUserInfo(popupProfile1.getInputValues());
   popupProfile1.close();
 });
+popupProfile1.setEventListeners();
 
 // открытие попапа профиля
 buttonEditPopup.addEventListener("click", () => {
   popupProfile1.open();
-  popupProfile1.setEventListeners();
-
-  popupProfile1.setInputValues(UserProfile.getUserInfo());
+  popupProfile1.setInputValues(userProfile.getUserInfo());
   // сброс ошибок
   formValidPopupProfile.resetErorr();
 });
@@ -39,20 +38,21 @@ const popupCard1 = new PopupWithForm('.popup_card', (evt) => {
   cardList.addItem(cardList.renderer(popupCard1.getInputValues()));
   popupCard1.close();
 });
+popupCard1.setEventListeners();
 
 // открытие попапа создания карточек
 buttonAddCard.addEventListener("click", () => {
   popupCard1.open();
-  popupCard1.setEventListeners();
   // сброс ошибок
   formValidpopupCard.resetErorr();
 });
 
+const image = new PopupWithImage('.popup_image-open');
+image.setEventListeners();
+
 //! функция для связи Card c попапом
 const handleCardClick = (objectData) => {
-  const image = new PopupWithImage('.popup_image-open');
   image.open(objectData);
-  image.setEventListeners();
 };
 
 //создание экземпляра класса с карточками
